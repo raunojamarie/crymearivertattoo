f53b2613942f9d73bd621669acfa754ac4508479
RaunoPaluvee <rauno.paluvee@guardtime.com>
2023-11-02 22:48:27 +0200

    big changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
-import plakat from "./assets/plakat-resized-cropped.jpg";
+import plakat from "./assets/Pilt4.jpg";
 import logo from "./assets/logo.png";
+import headerText from "./assets/header.png";
+import tattooFest from "./assets/tattoofest.png";
+import { FlipDate } from './FlipDate';
 
 function App() {
+
   const ORANGE_COLOR = "#fb8300";
   return (
     <div className="app-root">
       <div className="navbar" style={{
         position: "fixed",
         top: 0, right: 0, left: 0,
-        height: 200,
-        backgroundColor: "rgb(0, 0, 0, 0.8)",
+        height: 100,
+        backgroundColor: "black",
         display: "flex",
-        paddingLeft: 300,
-        paddingRight: 300
+        paddingLeft: 40,
+        paddingRight: 40,
+        zIndex: 999
       }}>
         <div className="navbar-menu" style={{
           display: "flex",
@@ -23,19 +28,34 @@ function App() {
           justifyContent: "center",
           color: "white"
         }}>
-          <div className="navbar-logo" style={{ width: 200, height: 200, display: "flex", paddingRight: 60}}>
-           <img alt="" src={logo} style={{ height: "100%", display: "block", margin: "auto", borderRadius: "50%" }}></img>
+          <div className="navbar-logo" style={{ width: 100, height: 100, display: "flex", paddingRight: 60, color: ORANGE_COLOR}}>
+            <p>crymearivertattoo</p>
           </div>
-          <p>AVALEHT</p>
-          <p>KAVA</p>
-          <p>ARTISTID</p>
-          <p>HÄÄLETUS</p>
-          <p>GALERII</p>
-          <p>KONTAKT</p>
+          <div style={{ fontSize: 20, display: "flex", width: "100%", justifyContent: "end", textTransform: "uppercase" }}>
+            <p>AVALEHT</p>
+            <p>ARTISTID</p>
+            <p>RAHVAHÄÄLETUS</p>
+            <p>TARTU 2024</p>
+            <p>KONTAKT</p>
+            <p>ET</p>
+          </div>
         </div>
       </div>
-      <div className="main-header" style={{ width: "100%", height: "100%"}}>
-        <img src={plakat} alt="" style={{ width: "100%" }} />
+      <div className="main-header" style={{ display: "flex", width: "100%", height: "100%" }}>
+        <img src={plakat} alt="" style={{ width: "100%", height: "100vh", marginLeft: 600 }} />
+
+        <div style={{ position: "absolute", top: 160, left: 100, width: 800 }}>
+          <img style={{ width: "100%" }} src={headerText}></img>
+          <img style={{ marginTop: "-90px", width: "100%" }} src={tattooFest}></img>
+        </div>
+        <div className="counter-wrapper" style={{ position: "absolute", top: 360, left: 1020 }}>
+          <p style={{ paddingLeft: 12, color: "white", padding: 0, margin: 0, fontSize: 20}}>ALGAB JUBA</p>
+          <FlipDate />
+          <div style={{ paddingRight: 12, display: "grid", justifyContent: "end" }}>
+            <p style={{ color: "white", padding: 0, margin: 0, fontSize: 20}}>PÄRAST</p>
+          </div>
+        </div>
+
       </div>
       <div className="main-content" style={{ height: 2400}}>
         <div></div>
