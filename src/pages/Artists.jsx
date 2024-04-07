2745aae7ba19831f16b9d8b9cee0f616a37750df
Marie Vabamägi <marievabamagi@Maries-Air.lan>
2024-04-07 16:02:59 +0300

    artist portraits

--- src/pages/Artists.jsx
+++ src/pages/Artists.jsx
@@ -5,6 +5,30 @@ import img2 from "../assets/Marta-Liisa.jpg"
 import img3 from "../assets/Santo-Sueno.JPG"
 import img4 from "../assets/Michael-Stade.JPG"
 import img5 from "../assets/Janis-Korkjas.JPG"
+import img6 from "../assets/Arina.jpeg"
+import img7 from "../assets/Blackgoat- Daughter.jpg"
+import img8 from "../assets/Doris-Rosenberg.JPG"
+import img9 from "../assets/Ele-Helina- Sein.jpg"
+import img10 from "../assets/Elvo-Grimm.jpg"
+import img11 from "../assets/Iisak.jpg"
+import img12 from "../assets/Janina.jpeg"
+import img13 from "../assets/Kerli-Laidna.jpg"
+import img14 from "../assets/Kerli-Redfern.JPG"
+import img15 from "../assets/Krall.jpg"
+import img16 from "../assets/Laura-glassd0ll.jpg"
+import img17 from "../assets/Mari-murikaz.jpg"
+import img18 from "../assets/Nico.jpeg"
+import img19 from "../assets/Paul-Eerik Rosenberg.jpeg"
+import img20 from "../assets/Ramon-Rattasep.JPG"
+import img21 from "../assets/silvia_tattoos.JPG"
+import img22 from "../assets/Kristjan-Aosaar.jpeg"
+import img23 from "../assets/Ulrike.jpg"
+import img24 from "../assets/Valerie.jpg"
+import img25 from "../assets/valuhaldjas.jpg"
+import img26 from "../assets/Darjapxl.jpg"
+import img27 from "../assets/Hatsu.jpg"
+import img28 from "../assets/Emilia-Sabirova.jpeg"
+import img29 from "../assets/Darius.jpg"
 import useWindowDimensions from "../useWindowDimensions";
 import '../styles/artists.css';
 import { useState } from "react";
@@ -38,124 +62,139 @@ const ARTISTS_DATA = [
     img: img5
   },
   {
-    name: "Paul-Eerik Rosenberg",
-    link: <a href="https://www.instagram.com/rosenbergpaul_tattoo" target="blank" className="link">@rosenbergpaul_tattoo</a>,
-    img: stock2
+    name: "Arina Stretškova",
+    link: <a href="https://www.instagram.com/barbiesundaytattoos" target="blank" className="link">@barbiesundaytattoos</a>,
+    img: img6
+  },
+  {
+    name: "Ave-Mari",
+    link: <a href="https://www.instagram.com/blackgoatsdaughter" target="blank" className="link">@blackgoatsdaughter</a>,
+    img: img7
   },
   {
     name: "Doris Rosenberg",
     link: <a href="https://www.instagram.com/dorisrosenberg_tattoo" target="blank" className="link">@dorisrosenberg_tattoo</a>,
-    img: stock1
+    img: img8
+  },
+  {
+    name: "Ele-Helina Sein",
+    link: <a href="https://www.instagram.com/redserpenttats" target="blank" className="link">@redserpenttats</a>,
+    img: img9
+  },
+  {
+    name: "Elvo Grimm",
+    link: <a href="https://www.instagram.com/grimme87" target="blank" className="link">@grimme87</a>,
+    img: img10
   },
   {
     name: "Iisak Pilli",
     link: <a href="https://www.instagram.com/impossible_son" target="blank" className="link">@impossible_son</a>,
-    img: stock2
+    img: img11
   },
   {
-    name: "Kaarel Krall",
-    link: <a href="https://www.instagram.com/_krlkrl_" target="blank" className="link">@_krlkrl_</a>,
-    img: stock1
+    name: "Janina Jakobson",
+    link: <a href="http://www.instagram.com/jj13.tattoo" target="blank" className="link">@jj13.tattoo</a>,
+    img: img12
   },
   {
-    name: "Arina Stretškova",
-    link: <a href="https://www.instagram.com/barbiesundaytattoos" target="blank" className="link">@barbiesundaytattoos</a>,
-    img: stock2
-  },
-  {
-    name: "Kristjan Aosaar",
-    link: <a href="https://www.instagram.com/ignoramus_maximus" target="blank" className="link">@ignoramus_maximus</a>,
-    img: stock1
-  },
-  {
-    name: "Ants Rauba",
-    link: <a href="" target="blank" className="link">@</a>,
-    img: stock1
-  },
-  {
-    name: "Edmar Sulaoja",
-    link: <a href="" target="blank" className="link">@</a>,
-    img: stock2
-  },
-  {
-    name: "Vitaly Makurin",
-    link: <a href="" target="blank" className="link">@</a>,
-    img: stock1
-  },
-  {
-    name: "Liisa Kivimäe",
-    link: <a href="https://www.instagram.com/valuhaldjas" target="blank" className="link">@valuhaldjas</a>,
-    img: stock2
-  },
-  {
-    name: "Valeriya Gilimson",
-    link: <a href="https://www.instagram.com/bitethegatto" target="blank" className="link">@bitethegatto</a>,
-    img: stock1
-  },
-  {
-    name: "Allan Tuul",
-    link: <a href="https://www.instagram.com/zero.kaos" target="blank" className="link">@zero.kaos</a>,
-    img: stock2
-  },
-  {
-    name: "",
-    link: <a href="https://www.instagram.com/rottenbonestattoo" target="blank" className="link">@rottenbonestattoo</a>,
-    img: stock1
-  },
-  {
-    name: "",
-    link: <a href="https://www.instagram.com/redserpenttats" target="blank" className="link">@redserpenttats</a>,
-    img: stock2
-  },
-  {
-    name: "",
+    name: "Kerli Laidna",
     link: <a href="https://www.instagram.com/mor.inkk" target="blank" className="link">@mor.inkk</a>,
-    img: stock2
-  },
-  {
-    name: "",
-    link: <a href="https://www.instagram.com/grimme87" target="blank" className="link">@grimme87</a>,
-    img: stock2
-  },
-  {
-    name: "Ave-Mari( Blackgoat’s Daughter)",
-    link: <a href="https://www.instagram.com/blackgoatsdaughter" target="blank" className="link">@blackgoatsdaughter</a>,
-    img: stock2
-  },
-  {
-    name: "Dmitri (Hatsu)",
-    link: <a href="https://www.instagram.com/hatsu_blckcraft" target="blank" className="link">@hatsu_blckcraft</a>,
-    img: stock2
-  },
-  {
-    name: "Darja ( Pxl.ink)",
-    link: <a href="https://www.instagram.com/pxl.ink" target="blank" className="link">@pxl.ink</a>,
-    img: stock2
-  },
-  {
-    name: "Ulrike Brett Uukkivi",
-    link: <a href="https://www.instagram.com/uuk.kivi" target="blank" className="link">@uuk.kivi</a>,
-    img: stock2
-  },
-  {
-    name: "Mari Uibo",
-    link: <a href="https://www.instagram.com/murikaz" target="blank" className="link">@murikaz</a>,
-    img: stock2
-  },
-  {
-    name: "Laura Käst",
-    link: <a href="https://www.instagram.com/glassd0ll" target="blank" className="link">@glassd0ll</a>,
-    img: stock2
+    img: img13
   },
   {
     name: "Kerli Luure",
     link: <a href="https://www.instagram.com/redfern.ink" target="blank" className="link">@redfern.ink</a>,
+    img: img14
+  },
+  {
+    name: "Kaarel Krall",
+    link: <a href="https://www.instagram.com/_krlkrl_" target="blank" className="link">@_krlkrl_</a>,
+    img: img15
+  },
+  {
+    name: "Laura Käst",
+    link: <a href="https://www.instagram.com/glassd0ll" target="blank" className="link">@glassd0ll</a>,
+    img: img16
+  },
+  {
+    name: "Mari Uibo",
+    link: <a href="https://www.instagram.com/murikaz" target="blank" className="link">@murikaz</a>,
+    img: img17
+  },
+  {
+    name: "Nico Fibbioli",
+    link: <a href="https://www.instagram.com/nicogrind_tattoo" target="blank" className="link">@Nicogrind_tattoo </a>,
+    img: img18
+  },
+  {
+    name: "Paul-Eerik Rosenberg",
+    link: <a href="https://www.instagram.com/rosenbergpaul_tattoo" target="blank" className="link">@rosenbergpaul_tattoo</a>,
+    img: img19
+  },
+  {
+    name: "Ramon Rattasep",
+    link: <a href="https://www.instagram.com/rottenbonestattoo" target="blank" className="link">@rottenbonestattoo</a>,
+    img: img20
+  },
+  {
+    name: "Silvia Volmar",
+    link: <a href="https://www.instagram.com/silvia_tattoos" target="blank" className="link">@silvia_tattoos</a>,
+    img: img21
+  },
+  {
+    name: "Kristjan Aosaar",
+    link: <a href="https://www.instagram.com/ignoramus_maximus" target="blank" className="link">@ignoramus_maximus</a>,
+    img: img22
+  },
+  {
+    name: "Ulrike Brett Uukkivi",
+    link: <a href="https://www.instagram.com/uuk.kivi" target="blank" className="link">@uuk.kivi</a>,
+    img: img23
+  },
+  {
+    name: "Valeriya Gilimson",
+    link: <a href="https://www.instagram.com/bitethegatto" target="blank" className="link">@bitethegatto</a>,
+    img: img24
+  },
+  {
+    name: "Liisa Kivimäe",
+    link: <a href="https://www.instagram.com/valuhaldjas" target="blank" className="link">@valuhaldjas</a>,
+    img: img25
+  },
+  {
+    name: "Darja",
+    link: <a href="https://www.instagram.com/pxl.ink" target="blank" className="link">@pxl.ink</a>,
+    img: img26
+  },
+  {
+    name: "Dmitri Hatsu",
+    link: <a href="https://www.instagram.com/hatsu_blckcraft" target="blank" className="link">@hatsu_blckcraft</a>,
+    img: img27
+  },
+  {
+    name: "Emilia Sabirova",
+    link: <a href="https://www.instagram.com/inkyshrimp" target="blank" className="link">@inkyshrimp</a>,
+    img: img28
+  },
+  {
+    name: "Darius Cloud",
+    link: <a href="https://www.instagram.com/tattoodarius" target="blank" className="link">@tattoodarius</a>,
+    img: img29
+  },
+  {
+    name: "Ants Rauba",
+    link: <a href="https://www.instagram.com/antsrauba" target="blank" className="link">@antsrauba</a>,
+    img: stock1
+  },
+  {
+    name: "Edmar Sulaoja",
+    link: <a href="https://www.instagram.com/etstattoo" target="blank" className="link">@etstattoo</a>,
     img: stock2
   },
   {
-    name: "Silvia Volmar ",
-    link: <a href="https://www.instagram.com/silvia_tattoos" target="blank" className="link">@silvia_tattoos</a>,
-    img: stock2
+    name: "Vitaly Makurin",
+    link: <a href="https://www.instagram.com/vitaly_makurin" target="blank" className="link">@vitaly_makurin</a>,
+    img: stock1
   },
   {
     name: "Roman Sestakov",
@@ -238,11 +277,6 @@ const ARTISTS_DATA = [
     img: stock2
   },
   {
-    name: "Nico Fibbioli",
-    link: <a href="https://www.instagram.com/nicogrind_tattoo" target="blank" className="link">@Nicogrind_tattoo </a>,
-    img: stock2
-  },
-  {
     name: "Lorenzo Gentil ",
     link: <a href="https://www.instagram.com/lorenzogentil" target="blank" className="link">@lorenzogentil</a>,
     img: stock2
@@ -253,8 +287,13 @@ const ARTISTS_DATA = [
     img: stock2
   },
   {
-    name: "Emilia Sabirova",
-    link: <a href="https://www.instagram.com/inkyshrimp" target="blank" className="link">@inkyshrimp</a>,
+    name: "Merili Mägi",
+    link: <a href="https://www.instagram.com/illusiooniline" target="blank" className="link">@illusiooniline</a>,
+    img: stock2
+  },
+  {
+    name: "Tilda Mae",
+    link: <a href="https://www.instagram.com/tildatattoo" target="blank" className="link">@tildatattoo</a>,
     img: stock2
   },
   {
@@ -332,7 +371,7 @@ const Artists = () => {
                       <div className="circular" style={{ display: 'flex', flexDirection: 'column' }}>
                         <img className="artist-img" alt="" src={artist.img}></img>
                       </div>
-                      <p className="artists-sub-title" style={{ fontSize: isMobile ? '24px' : '24px', marginTop: '20px', display: 'flex', justifyContent: 'center' }}>{artist.name}</p>
+                      <p className="artists-sub-title" style={{ fontSize: isMobile ? '18px' : '18px', marginTop: '20px', display: 'flex', justifyContent: 'center' }}>{artist.name}</p>
                       <div>
                         <p className="text-black container" style={{ fontSize: isMobile ? '14px' : '18px', marginTop: '10px', width: '200px', display: 'flex', justifyContent: 'center' }}>
                           {artist.link}
